93981df7b9a05d59b9c8e6774734513c0d7d3e99
ivankatsan24954-oss <ivankatsan24954@gmail.com>
2026-07-05 15:09:27 +0900

    Add files via upload

--- car.js
+++ car.js
@@ -358,16 +358,6 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-const closeMenuBtn = document.getElementById(`close-menu`);
-const menuOverlay = document.getElementById(`menu-overlay`);
-
-if (closeMenuBtn && menuOverlay) {
-   closeMenuBtn.addEventListener(`click`, (e) => {
-      e.preventDefault();
-      menuOverlay.setAttribute(`hiddent`, ``);
-   });
-}
-
 // ---------- Инициализация ----------
 
 renderCarDetail().catch(err => {
